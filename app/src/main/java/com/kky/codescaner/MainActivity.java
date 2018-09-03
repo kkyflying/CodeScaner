37af5450be27a3c3308d3dc0b851210501c7b12b
kky <kkyflying@gmail.com>
2018-09-04 00:06:43 +0800

    修改版本

--- app/src/main/java/com/kky/codescaner/MainActivity.java
+++ app/src/main/java/com/kky/codescaner/MainActivity.java
@@ -10,7 +10,6 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
