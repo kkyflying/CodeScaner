0c4869cddbcd625654c40ec99cb51f9f0104b989
KKy <kkyflying@gmail.com>
2019-06-06 11:39:54 +0800

    1.更新版本号 2.添加复制到键盘 3.更新ui

---
+++ app/src/main/java/com/kky/codescaner/Constant.java
@@ -0,0 +1,18 @@
+package com.kky.codescaner;
+
+/**
+ * Author: zzk
+ * Date: 2019/06/06
+ * Time: 11:31
+ * Description:
+ */
+public class Constant {
+
+    public static final int REQ_QRCODE = 0x1111;
+
+
+    public static final String URL_HOME = "https://github.com/kkyflying/CodeScaner";
+
+    public static final String URL_BOLG = "https://kkyflying.github.io/index";
+
+}
--- app/src/main/java/com/kky/codescaner/MainActivity.java
+++ app/src/main/java/com/kky/codescaner/MainActivity.java
@@ -1,6 +1,8 @@
 package com.kky.codescaner;
 
 import android.Manifest;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -10,6 +12,7 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -27,11 +30,6 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    public static final int REQ_QRCODE = 0x1111;
-
-
-    public static final String url= "https://github.com/kkyflying/CodeScaner";
-
     @BindView(R.id.btnScaner)
     Button btnScaner;
     @BindView(R.id.tvReuslt)
@@ -40,7 +38,10 @@ public class MainActivity extends AppCompatActivity {
     ImageView iamge1;
     @BindView(R.id.iamge2)
     ImageView iamge2;
+    @BindView(R.id.btnCopy)
+    Button btnCopy;
 
+    private ClipboardManager mClipboardManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,10 +52,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init() {
+        mClipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.k);
-        iamge1.setImageBitmap(EncodingHandler.createQRCode(url, 500, 500, bitmap));
+        iamge1.setImageBitmap(EncodingHandler.createQRCode(Constant.URL_HOME, 500, 500, bitmap));
         try {
-            iamge2.setImageBitmap(EncodingHandler.createQRCode(url,500));
+            iamge2.setImageBitmap(EncodingHandler.createQRCode(Constant.URL_BOLG,500));
         } catch (WriterException e) {
             e.printStackTrace();
         }
@@ -62,8 +65,15 @@ public class MainActivity extends AppCompatActivity {
 
 
     @OnClick(R.id.btnScaner)
-    public void onViewClicked() {
+    public void onViewClickedScaner() {
         getPermission();
+    }
+
+    @OnClick(R.id.btnCopy)
+    public void onViewClickedCopy() {
+        ClipData clipData = ClipData.newPlainText("code", tvReuslt.getText());
+        mClipboardManager.setPrimaryClip(clipData);
+        Toast.makeText(this,"copy ~ ",Toast.LENGTH_SHORT).show();
     }
 
     private void getPermission() {
@@ -73,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 2);
         } else {
             //权限已经被授予，在这里直接写要执行的相应方法即可
-            startActivityForResult(new Intent(MainActivity.this, CaptureActivity.class), REQ_QRCODE);
+            startActivityForResult(new Intent(MainActivity.this, CaptureActivity.class), Constant.REQ_QRCODE);
         }
     }
 
@@ -81,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode == 2) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                startActivityForResult(new Intent(MainActivity.this, CaptureActivity.class), REQ_QRCODE);
+                startActivityForResult(new Intent(MainActivity.this, CaptureActivity.class), Constant.REQ_QRCODE);
             } else {
                 // Permission Denied
                 Toast.makeText(MainActivity.this,"Permission Denied",Toast.LENGTH_SHORT).show();
@@ -93,11 +103,19 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK && requestCode == REQ_QRCODE && data != null) {
+        if (resultCode == RESULT_OK && requestCode == Constant.REQ_QRCODE && data != null) {
             byte[] result = data.getByteArrayExtra(CaptureActivity.KEY_RESULT);
-            if (result == null || result.length == 0) return;
+            if (result == null || result.length == 0){
+                tvReuslt.setText("");
+                btnCopy.setVisibility(View.GONE);
+                return;
+            }
             String payCode = new String(result);
             tvReuslt.setText(payCode);
+            btnCopy.setVisibility(View.VISIBLE);
+        }else {
+            tvReuslt.setText("");
+            btnCopy.setVisibility(View.GONE);
         }
     }
 
